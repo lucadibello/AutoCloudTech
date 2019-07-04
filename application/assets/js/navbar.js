c798dc19580f517eb2411dda64d54222d5032052
Matteo Forni <37305596+matteoforni@users.noreply.github.com>
2019-07-04 10:18:34 +0200

    modify navbar.js

--- application/assets/js/navbar.js
+++ application/assets/js/navbar.js
@@ -1,12 +1,20 @@
-jQuery(document).ready(function($) {
-    $(window).scroll(function() {
+jQuery(document).ready(function ($) {
+    $(window).scroll(function () {
         var scrollPos = $(window).scrollTop();
-        var navbar = $('.navbar');
-  
-      if (scrollPos > 400) {
-        navbar.addClass('default-color');
-      } else {
-        navbar.removeClass('default-color');
-      }
+        var navbar = $('#navigation');
+
+        if (scrollPos > 400) {
+            console.log("scroll down");
+            navbar.addClass('.warning-color');
+            /*navbar.addClass('.animated');
+            navbar.addClass('.fadeIn');
+            navbar.addClass('.slow');*/
+        } else {
+            console.log("scroll up");
+            navbar.removeClass('.warning-color');
+            /*navbar.removeClass('.animated');
+            navbar.removeClass('.fadeIn');
+            navbar.removeClass('.slow');*/
+        }
     });
-  });
+});
