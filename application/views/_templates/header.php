cceecc9efd05198a2794ee37515c17a754ff5e5e
Matteo Forni <37305596+matteoforni@users.noreply.github.com>
2019-07-04 10:57:59 +0200

    moved links to right

--- application/views/_templates/header.php
+++ application/views/_templates/header.php
@@ -26,11 +26,13 @@
 
 <body>
     <nav id="navigation" class="navbar navbar-expand-lg fixed-top text-white scrolling-navbar">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">
+            <img src="./application/assets/img/logo.jpg" height="30" alt="ACT logo">
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item active">
                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
