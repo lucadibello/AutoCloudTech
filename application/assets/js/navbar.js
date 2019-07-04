1baadece70c23844e5ef7703942727de31e8a799
Matteo Forni <37305596+matteoforni@users.noreply.github.com>
2019-07-04 10:42:20 +0200

    fix bug in navbar.js

--- application/assets/js/navbar.js
+++ application/assets/js/navbar.js
@@ -4,17 +4,10 @@ jQuery(document).ready(function ($) {
         var navbar = $('#navigation');
 
         if (scrollPos > 400) {
-            console.log("scroll down");
-            navbar.addClass('.warning-color');
-            /*navbar.addClass('.animated');
-            navbar.addClass('.fadeIn');
-            navbar.addClass('.slow');*/
+            navbar.addClass('warning-color');
+            document.getElementById("navigation").style.transition = "all 1s";
         } else {
-            console.log("scroll up");
-            navbar.removeClass('.warning-color');
-            /*navbar.removeClass('.animated');
-            navbar.removeClass('.fadeIn');
-            navbar.removeClass('.slow');*/
+            navbar.removeClass('warning-color');
         }
     });
 });
