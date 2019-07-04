6a1e97977de0a8f1193e786b0eae8eaba5be87ff
Luca Di Bello <luca.dibello@samtrevano.ch>
2019-07-04 15:09:47 +0100

    Fix swiper

--- application/views/_templates/footer.php
+++ application/views/_templates/footer.php
@@ -89,13 +89,34 @@
             effect: 'coverflow',
             grabCursor: true,
             centeredSlides: true,
-            slidesPerView: 'auto',
+            slidesPerView: 2,
+            resizeReInit: true,
+            autoHeight: true,
+            spaceBetween: 100,
             coverflowEffect: {
-                rotate: 50,
+                rotate: 55,
                 stretch: 0,
-                depth: 100,
+                depth: 300,
                 modifier: 1,
                 slideShadows: true,
+            },
+            breakpoints: {
+                // when window width is <= 320px     
+                320: {
+                    slidesPerView: 1,
+                    spaceBetween: 10
+                },
+                // when window width is <= 480px     
+                480: {
+                    slidesPerView: 2,
+                    spaceBetween: 20
+                },
+
+                // when window width is <= 640px     
+                640: {
+                    slidesPerView: 3,
+                    spaceBetween: 30
+                }
             },
             pagination: {
                 el: '.swiper-pagination',
