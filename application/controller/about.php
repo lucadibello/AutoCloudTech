c58bc52ef1463811427aa137693b82d9597098b1
Matteo Forni <37305596+matteoforni@users.noreply.github.com>
2019-07-05 09:02:17 +0200

    create controller about

---
+++ application/controller/about.php
@@ -0,0 +1,11 @@
+<?php
+class About
+{
+    public function index()
+    {
+        //Show index page
+        ViewLoader::load("_templates/header");
+        ViewLoader::load("about/index");
+        ViewLoader::load("_templates/footer");
+    }
+}
