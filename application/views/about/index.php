29b51227ae73c79a0337e99c4b664d4218f64d0a
Matteo Forni <37305596+matteoforni@users.noreply.github.com>
2019-07-05 09:51:14 +0200

    update about

--- application/views/about/index.php
+++ application/views/about/index.php
@@ -1,17 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 </head>
+
 <body>
+    <div class="view jarallax" style="height: 100vh;">
+        <img class="jarallax-img" src="https://images.unsplash.com/photo-1502747812021-0ae746b6c23f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="">
+        <div class="mask rgba-blue-slight">
+            <div class="container flex-center text-center">
+                <div class="row mt-5">
+                    <div class="col-md-12 mb-3 animated slideInDown">
+                        <h1 class="title h1 my-3 font-weight-bold">Auto<span class="red-text">Cloud</span><span class="indigo-text">Tech</span></h1>
+                        <h5 class="text-uppercase mb-3 mt-1 font-weight-bold ">Web developer & graphic designer</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-            aaaaaaaaaaaaaaaaaaaaaaS
+                <section class="my-5">
+                    <h2 class="h1-responsive font-weight-bold text-center my-5">Our System?</h2>
+                    <p class="lead grey-text text-center w-responsive mx-auto mb-5">
+                        Auto Cloud Tech supplies it's SaaS based ERP system to tier 3 Auto Factors (1 to 20 branches) in the Irish 
+                        and UK automotive aftermarket. 
+                        <span id="moreSystem"> 
+                            The technology was developed in close collaboration with a Cork based auto factor, 
+                            Premier Auto Parts who currently use it as the primary ERP system in their multiple stores.
+                        </span>
+                    </p>
+                </section>
             </div>
         </div>
     </div>
 </body>
+
 </html>
