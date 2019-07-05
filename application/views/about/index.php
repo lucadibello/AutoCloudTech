1a9012409fe1856f6188892ea20394a10d59e438
Matteo Forni <37305596+matteoforni@users.noreply.github.com>
2019-07-05 10:28:49 +0200

    update about index

--- application/views/about/index.php
+++ application/views/about/index.php
@@ -35,18 +35,43 @@
                         </span>
                         <button type="button" id="systemBtn" class="btn" name="moreSystem" onclick="showMore(this.id, this.name)">Read more</button>
                     </p>
+                </section>
                 </section class="my-5">
                     <h2 class="h1-responsive font-weight-bold text-center my-5">Challenges for IT companies in Aftermarket Industry</h2>
                     <p class="lead grey-text text-center w-responsive mx-auto mb-5">
                         It is widely accepted that the aftermarket motor industry is very complex for which to develop Cloud based software.<br>
                         <span id="moreChallenge"> 
-                        There are over 23,000 car makes and models in circulation, some of which have been modified. 
-                        Each car has over 10,000 parts that are constantly being replaced, upgraded and moved to end of life.<br>
+                            There are over 23,000 car makes and models in circulation, some of which have been modified. 
+                            Each car has over 10,000 parts that are constantly being replaced, upgraded and moved to end of life.<br>
                         </span>
                         <button type="button" id="challengeBtn" class="btn" name="moreChallenge" onclick="showMore(this.id, this.name)">Read more</button>
                     </p>
                 <section>
-                </section>
+                </section class="my-5">
+                    <h2 class="h1-responsive font-weight-bold text-center my-5">Site Building Process</h2>
+                    <p class="lead grey-text text-center w-responsive mx-auto mb-5">
+                        The directors of Auto Cloud Tech (ACT) were commissioned to build a full ERP system, for Premier Auto Parts, in the autumn of 2015.<br>
+                        <span id="moreSiteBuilding"> 
+                            It was key that we undertook the role of a motor factor for this phase of development. We needed to be flexible enough to integrate into the business without being a burden on it's operations.
+                            If a large software development firm was brought into develop a similar system, the interruption on the business would impact negatively.
+                            <br>
+                        </span>
+                        <button type="button" id="siteBuildingBtn" class="btn" name="moreSiteBuilding" onclick="showMore(this.id, this.name)">Read more</button>
+                    </p>
+                <section>
+                </section class="my-5">
+                    <h2 class="h1-responsive font-weight-bold text-center my-5">Minimising Disruption</h2>
+                    <p class="lead grey-text text-center w-responsive mx-auto mb-5">
+                        We set up, Auto Factors Direct (AFD) as a 100% online, trade only, auto parts company. This was the mechanism to develop and perfect the modules before implementation with Premier Auto Parts. It also ensured that there was no disruption to Premier’s day to day business.<br>
+                        <span id="moreDisruption"> 
+                        AFD, using the Auto Cloud Tech system as their primary ERP, turned over €260k in 2016 and €1.4m in it's first full trading year 2017.
+                        It was developed over a two-year period in conjunction with Premier Auto Parts, in their Cork offices, where they allowed the development team full access to the business to ascertain the requirements for the various sectors of their organisation.
+                        We were able to benefit from real time customer feedback and involved their expertise in every step of the systems development. During the building process Premier requested the introduction of a number of bespoke modules which were not previously available to the tier 3 motor factor.
+                        <br>
+                        </span>
+                        <button type="button" id="disruptionBtn" class="btn" name="moreDisruption" onclick="showMore(this.id, this.name)">Read more</button>
+                    </p>
+                <section>
             </div>
         </div>
     </div>
