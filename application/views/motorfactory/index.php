7916e49c1975f677193079ad8616b7f475946461
Matteo Forni <37305596+matteoforni@users.noreply.github.com>
2019-07-09 08:51:34 +0200

    update motorfactory view

--- application/views/motorfactory/index.php
+++ application/views/motorfactory/index.php
@@ -21,6 +21,32 @@
             </div>
         </div>
     </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 text-center">
+                <div id="website247">
+                    <img src="./assets/img/icons/website247.png" alt="website">
+                </div>
+            </div>
+            <div class="col-md-4 text-center">
+                
+            </div>
+            <div class="col-md-4 text-center">
+                
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 text-center">
+
+            </div>
+            <div class="col-md-4 text-center">
+                
+            </div>
+            <div class="col-md-4 text-center">
+                
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
