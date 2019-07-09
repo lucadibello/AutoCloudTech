b6f1bfccb56c52b31e4938104c9aa69c3d48611c
Matteo Forni <37305596+matteoforni@users.noreply.github.com>
2019-07-09 08:37:07 +0200

    create motorfactory view

---
+++ application/views/motorfactory/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Motor Factory Software System</title>
+</head>
+
+<body>
+    <div class="view jarallax" style="height: 60vh;">
+        <img class="jarallax-img" src="https://images.unsplash.com/photo-1561404679-3657b0ad8dac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="">
+        <div class="mask rgba-blue-slight">
+            <div class="container flex-center text-center">
+                <div class="row mt-5">
+                    <div class="col-md-12 mb-3 animated slideInDown">
+                        <h1 class="title h1 my-3 font-weight-bold">Auto<span class="red-text">Cloud</span><span class="indigo-text">Tech</span></h1>
+                        <h5 class="text-uppercase mb-3 mt-1 font-weight-bold ">Web developer & graphic designer</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
