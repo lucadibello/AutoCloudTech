e3bbae1a7f178638c96521806fdca9f0322d50de
Luca Di Bello <luca.dibello@samtrevano.ch>
2019-07-09 09:46:27 +0100

    New logo + update navbar

--- application/assets/js/navbar.js
+++ application/assets/js/navbar.js
@@ -6,8 +6,12 @@ jQuery(document).ready(function($) {
         if (scrollPos > 400) {
             document.getElementById('navigation').style.backgroundColor = "#FC4445";
             document.getElementById("navigation").style.transition = "all 0.7s";
+
+            $("#nav-image").attr("src","./application/assets/illustrator/logo-blue.svg");
         } else {
             document.getElementById('navigation').style.backgroundColor = "transparent";
+
+            $("#nav-image").attr("src","./application/assets/illustrator/logo.svg");
         }
     });
 });
